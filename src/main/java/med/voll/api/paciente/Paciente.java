0cce0b09d53e19d2cd90c5f838493f86b8a12fd7
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2024-04-15 14:55:01 -0300

    api java spring atualização e deleção lógica

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -22,8 +22,10 @@ public class Paciente {
     private String cpf;
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Paciente(DadosCadastroPaciente dados){
+        this.ativo = true;
         this.nome= dados.nome();
         this.telefone= dados.telefone();
         this.cpf= dados.cpf();
@@ -31,4 +33,20 @@ public class Paciente {
         this.endereco =  new Endereco(dados.endereco());
     }
 
+    public void atualizaInformacoes(DadosAtualizacaoPaciente dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
